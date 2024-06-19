486ee7ebebd3a99dd6ce1bcb0830927409e0546a
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-06-19 17:35:06 -0230

    Created a new Query

--- SQL Queries/garret-query2.sql
+++ SQL Queries/garret-query2.sql
@@ -1,15 +1,13 @@
--- This query will get the employees 'pay_rate' and 'tax_percent' from the 'employees' table and the 'tax bracket' table.
--- The result will be grouped by 'employee_id' and will include the full name of the employee, 
--- 'pay_rate', 'job_title', 'tax_percent' for each employee.
+-- This query will bring up all the invoice with items on it from the invoice table and the invoice_x_items table
+-- and the items table. It will show the invoice date, the invoice number, the item name, the item price, and the
+-- quantity of the item. It will be ordered by the invoice date in descending order.
 
-SELECT employees.employee_id
-  ,employees.first_name || ' ' || employees.last_name AS full_name -- Concatenate first and last name
-  ,employees.job_title -- Job title of the employee
-  ,payroll.pay_rate -- Pay rate of the employee
-  ,tax_brackets.tax_percent -- Tax percent based on tax bracket
-FROM employees
-JOIN payroll -- Join the payroll table to get the pay rate
-  ON employees.employee_id = payroll.employee_id
-JOIN tax_brackets -- Join the tax_brackets table to get the tax percent
-  ON payroll.tax_bracket_id = tax_brackets.tax_bracket_id
-ORDER BY employees.employee_id ASC;
+SELECT invoice.invoice_date as Invoice Date -- Selecting the invoice date
+    ,invoice.invoice_number as Invoice Number -- Selecting the invoice number
+    ,items.item_name as Item Name -- Selecting the item name
+    ,items.item_price as Item Price -- Selecting the item price
+    ,inventory_invoice_xref.quantity as Quantity -- Selecting the quantity of the item
+FROM invoice 
+JOIN invoice_x_items ON invoice.invoice_number = inventory_invoice_xref.invoice_number
+JOIN inventory ON inventory_invoice_xref.item_id = inventory.item_id
+ORDER BY invoice.invoice_date DESC;

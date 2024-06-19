bb272c0d92fe4f0c1c7b9ed7f2a0de0688ebd8f0
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-06-19 13:42:52 -0230

    updated values for data

--- SQL inserts/companyVehicle.insert.sql
+++ SQL inserts/companyVehicle.insert.sql
@@ -1,4 +1,4 @@
-insert into companyVehicle (vehicle_id, license_id, vin, make, model, year, last_service_date) values (1, 10, '3FAHP0CG1CR015088', 'GMC', 'Suburban 1500', 1997, '2023-10-13 22:23:38');
-insert into companyVehicle (vehicle_id, license_id, vin, make, model, year, last_service_date) values (2, 5, '1GD01ZCG1DF396898', 'Porsche', '911', 2006, '2023-04-30 05:14:01');
-insert into companyVehicle (vehicle_id, license_id, vin, make, model, year, last_service_date) values (3, 3, '1G6DA8E54D0176785', 'Ford', 'E-Series', 1995, '2023-06-09 06:20:57');
-insert into companyVehicle (vehicle_id, license_id, vin, make, model, year, last_service_date) values (4, 11, 'WBABD33414P867088', 'Audi', 'A5', 2008, '2023-08-21 14:23:46');
+insert into companyVehicle (vehicle_id, license_id, vin, make, model, year, last_service_date) values (1, 5, '3FAHP0CG1CR015088', 'GMC', 'Suburban 1500', 1997, '2023-10-13 22:23:38');
+insert into companyVehicle (vehicle_id, license_id, vin, make, model, year, last_service_date) values (2, 5, '1GD01ZCG1DF396898', 'Ford', 'F-150', 2023, '2023-04-30 05:14:01');
+insert into companyVehicle (vehicle_id, license_id, vin, make, model, year, last_service_date) values (3, 5, '1G6DA8E54D0176785', 'Ford', 'F-150', 2023, '2023-06-09 06:20:57');
+insert into companyVehicle (vehicle_id, license_id, vin, make, model, year, last_service_date) values (4, 5, 'WBABD33414P867088', 'Caterpillar', '303', 2008, '2023-08-21 14:23:46');
--- SQL inserts/employee_licenses_xref.insert.sql
+++ SQL inserts/employee_licenses_xref.insert.sql
@@ -1,5 +1,5 @@
 insert into employee_licenses_xref (employee_id, license_id, expiry_date) values (1, 5, '2025-06-15 11:02:06');
-insert into employee_licenses_xref (employee_id, license_id, expiry_date) values (2, 1, '2025-02-21 02:11:59');
+insert into employee_licenses_xref (employee_id, license_id, expiry_date) values (2, 2, '2025-02-21 02:11:59');
 insert into employee_licenses_xref (employee_id, license_id, expiry_date) values (3, 5, '2025-01-29 11:23:13');
-insert into employee_licenses_xref (employee_id, license_id, expiry_date) values (4, 6, '2025-09-05 08:50:00');
-insert into employee_licenses_xref (employee_id, license_id, expiry_date) values (5, 1, '2025-04-26 01:39:45');
+insert into employee_licenses_xref (employee_id, license_id, expiry_date) values (4, 5, '2025-09-05 08:50:00');
+insert into employee_licenses_xref (employee_id, license_id, expiry_date) values (5, 5, '2025-04-26 01:39:45');
